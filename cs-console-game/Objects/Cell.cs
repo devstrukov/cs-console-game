7101a65aef5aaa40b4ef1f693032d901f7b71d05
vladimir <vstrukoff@yandex.ru>
2024-12-28 17:10:47 +0300

    Implement Cell

--- cs-console-game/Objects/Cell.cs
+++ cs-console-game/Objects/Cell.cs
@@ -1,8 +1,34 @@
+using cs_console_game.Enums;
 using cs_console_game.Interfaces;
 
 namespace cs_console_game.Objects;
 
 public class Cell : ICell
 {
-    
+    CellState _state = CellState.Default;
+    Tuple<int, int> _coordinates;
+    public void SetState(CellState state)
+    {
+        _state = state;
+    }
+
+    public CellState GetState()
+    {
+        return _state;
+    }
+
+    public void SetCoordinates(Tuple<int, int> coordinates)
+    {
+        _coordinates = coordinates;
+    }
+
+    public Tuple<int, int> GetCoordinates()
+    {
+        return _coordinates;
+    }
+
+    public bool CanBeOccupied()
+    {
+        return _state == CellState.Empty;
+    }
 }
